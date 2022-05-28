2b5c55082bc7e083d87b5f651d816e8f88893b14
lordfme <fortmax120@gmail.com>
2022-05-28 09:56:18 +0100

    upload files

--- AOC_01_report_repair.kts
+++
@@ -1,9 +0,0 @@
-val numbers: MutableList<Int> = mutableListOf()
-(1..200).forEach{ _ ->
-    numbers.add(Integer.parseInt(readln()))
-}
-
-for(i in numbers.indices)
-    for (j in i+1 until numbers.size)
-        if (numbers[i] + numbers[j] == 2020)
-            println(numbers[i] * numbers[j])
